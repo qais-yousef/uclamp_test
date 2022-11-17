cc815f928299dfc724d16ab8464805a8ec0c16ea
Qais Yousef <qyousef@layalina.io>
2022-11-17 01:04:47 +0000

    uclamp_test_thermal_pressure: Add dummy bpf counterpart
    And ensure we compile and load bpf correctly.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-TESTS=$(basename $(wildcard *.c))
+TESTS=$(basename $(filter-out $(wildcard *.bpf.c), $(wildcard *.c)))
+BPF_SKEL=$(subst .bpf.c,.skel.h, $(wildcard *.bpf.c))
 
 CFLAGS=-O2 -Werror -Wall -g
 INCLUDE=-I bpf/usr/include
@@ -6,10 +7,12 @@ LIBS=-lpthread bpf/usr/lib64/libbpf.a -lelf -lz
 
 ARCH ?= arm64
 
-%: %.bpf.c
-	clang $(CFLAGS) -target bpf -D__TARGET_ARCH_$(ARCH) $(INCLUDE) -c $< -o $@.bpf.o
-	llvm-strip -g $@.bpf.o
-	bpftool gen skeleton $@.bpf.o > $@.skel.h
+%.bpf.o: %.bpf.c
+	clang $(CFLAGS) -target bpf -D__TARGET_ARCH_$(ARCH) $(INCLUDE) -c $< -o $@
+	llvm-strip -g $@
+
+%.skel.h: %.bpf.o
+	bpftool gen skeleton $< > $@
 
 %: %.c
 	$(CC) $(CFLAGS) $(INCLUDE) -c $@.c -o $@.o
@@ -23,6 +26,7 @@ all:
 
 	bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
 
+	make $(BPF_SKEL)
 	make $(TESTS)
 
 clean:
--- uclamp_test_thermal_pressure.c
+++ uclamp_test_thermal_pressure.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include "sched.h"
 
+#include <bpf/libbpf.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -10,9 +11,11 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "uclamp_test_thermal_pressure.skel.h"
+
 #define NR_LOOPS	1000000
 
-bool done = false;
+static bool done = false;
 
 //#define DEBUG
 #ifdef DEBUG
@@ -40,17 +43,40 @@ static void *thread_loop(void *data)
 
 int main(int argc, char **argv)
 {
+	struct uclamp_test_thermal_pressure_bpf *skel;
 	pthread_t thread;
 	int ret;
+
+	skel = uclamp_test_thermal_pressure_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open and load BPF skeleton\n");
+		return 1;
+	}
+
+	ret = uclamp_test_thermal_pressure_bpf__load(skel);
+	if (ret) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+
+	ret = uclamp_test_thermal_pressure_bpf__attach(skel);
+	if (ret) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
 
 	ret = pthread_create(&thread, NULL, thread_loop, NULL);
 	if (ret) {
 		perror("Failed to create thread");
-		return EXIT_FAILURE;
+		goto cleanup;
 	}
+
+	sleep(5);
 
 	done = true;
 	pthread_join(thread, NULL);
 
-	return EXIT_SUCCESS;
+cleanup:
+	uclamp_test_thermal_pressure_bpf__destroy(skel);
+	return ret < 0 ? -ret : EXIT_SUCCESS;
 }
