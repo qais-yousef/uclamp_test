e9355a8bbe12e2aa60cb8695d3dbaca2fef4d9e3
Qais Yousef <qyousef@layalina.io>
2022-11-17 23:26:43 +0000

    uclamp_test_thermal_pressure: Update bpf to use sched_switch
    And count how many times we run on a cpu. We need to filter out for our
    pid still.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- uclamp_test_thermal_pressure.bpf.c
+++ uclamp_test_thermal_pressure.bpf.c
@@ -5,11 +5,32 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
-SEC("raw_tp/pelt_se_tp")
-int BPF_PROG(handle_pelt_se, struct sched_entity *se)
-{
-	int cpu = BPF_CORE_READ(se, cfs_rq, rq, cpu);
 
-	bpf_printk("[%d] Hello world!", cpu);
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 8192);
+	__type(key, int);
+	__type(value, int);
+} sched_switch SEC(".maps");
+
+
+SEC("raw_tp/sched_switch")
+int BPF_PROG(handle_sched_switch, bool preempt,
+	     struct task_struct *prev, struct task_struct *next,
+	     unsigned int prev_state)
+{
+	pid_t pid;
+	int cpu, i = 0;
+	int *count;
+
+	pid = BPF_CORE_READ(prev, pid);
+	cpu = BPF_CORE_READ(prev, cpu);
+
+	count = bpf_map_lookup_elem(&sched_switch, &cpu);
+	if (count)
+		i = *count + 1;
+	bpf_map_update_elem(&sched_switch, &cpu, &i, BPF_ANY);
+
+	bpf_printk("[%d] Hello world!: %d", cpu, i);
 	return 0;
 }
