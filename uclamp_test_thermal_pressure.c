e5ba2592bda540fa44d469dca2773e7a2f122a4b
Qais Yousef <qyousef@layalina.io>
2022-11-19 20:54:39 +0000

    uclamp_test_thermal_pressure: Catch failures and warnings
    Print out information when we detected wrong or suboptimal placement.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- uclamp_test_thermal_pressure.c
+++ uclamp_test_thermal_pressure.c
@@ -50,6 +50,8 @@ static int handle_rq_pelt_event(void *ctx, void *data, size_t data_sz)
 	struct rq_pelt_event *e = data;
 	static FILE *file = NULL;
 	static bool err_once = false;
+	unsigned long cap;
+	int i;
 
 	if (!file) {
 		file = fopen(CSV_FILE, "w");
@@ -62,6 +64,19 @@ static int handle_rq_pelt_event(void *ctx, void *data, size_t data_sz)
 		}
 		fprintf(stdout, "Created %s\n", CSV_FILE);
 		fprintf(file, "ts,cpu,util, capacity_orig, thermal_avg, uclamp_min,uclamp_max, overutilized\n");
+	}
+
+	if (e->uclamp_min > e->capacity_orig)
+		fprintf(stderr, "[%llu] Failed: uclamp_min > capacity_orig --::-- %lu > %lu\n", e->ts, e->uclamp_min, e->capacity_orig);
+
+	for_each_capacity(cap, i) {
+		if (e->uclamp_min <= cap && e->capacity_orig > cap)
+			fprintf(stderr, "[%llu] Warning: uclamp_min = %lu --::-- running on %lu instead of %lu\n", e->ts, e->uclamp_min, e->capacity_orig, cap);
+
+		if (cap < e->capacity_orig && e->capacity_orig - e->thermal_avg < cap) {
+			fprintf(stderr, "[%llu] Warning: capacity_inversion --::-- capacity_orig - thermal_avg < cap --::-- %lu - %lu (%lu) < %lu\n",
+				e->ts, e->capacity_orig, e->thermal_avg, e->capacity_orig - e->thermal_avg, cap);
+		}
 	}
 
 	fprintf(file, "%llu,%d,%lu, %lu, %lu, %lu,%lu, %d\n",
