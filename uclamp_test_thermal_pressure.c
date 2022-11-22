84ff5147d4658d1aded36d53a0cdcb93c9fc89d4
Qais Yousef <qyousef@layalina.io>
2022-11-22 00:31:47 +0000

    uclamp_test_thermal_pressure: Improve the do_work()
    So that it gets some reasonable utilization and not a util of 0 or 1.
    The goal is to see the impact on calculating energy difference.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- uclamp_test_thermal_pressure.c
+++ uclamp_test_thermal_pressure.c
@@ -4,6 +4,7 @@
 #include "sched.h"
 
 #include <bpf/libbpf.h>
+#include <math.h>
 #include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
@@ -39,9 +40,14 @@ struct capacities {
 
 static inline __attribute__((always_inline)) void do_work() {
 	int loops = NR_LOOPS;
+	int i = 1000000;
 
-	while (loops--)
+	while (loops--) {
+		while (i--)
+			sqrt(pow(i, i));
+		i = 1000000;
 		usleep(16000);
+	}
 }
 
 #define PELT_CSV_FILE	"uclamp_test_thermal_pressure_pelt.csv"
